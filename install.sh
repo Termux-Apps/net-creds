a9c03d659fa8a73bfea23b317c4e993c017ae7b9
Phone Metal <74254069+Phone-Metal@users.noreply.github.com>
2021-01-31 16:08:41 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,4 @@ echo "Installing"
 apt-get full-upgrade
 clear
 apt-get install git python2 -y
-git clone https://github.com/Termux-Apps/tnet-creds/ 
-cd tnet-creds
 python2 -m pip install -r requirements
